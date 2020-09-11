232144bc0993652c1f67059c05b8870133644685
unith16 <unithkumar@gmail.com>
2020-09-11 00:36:09 -0400

    Update SE_TABLES.sql

--- SE_TABLES.sql
+++ SE_TABLES.sql
@@ -1,20 +1,32 @@
 CREATE TABLE Member(
-	MemberId	INT NOT NULL AUTO_INCREMENT,
-	Name		VARCHAR(30) NOT NULL, 
-	DOB			DATE,
-	Languages	VARCHAR(100) NOT NULL,
-	IsAssigned	INT,
-	HourlyRate	FLOAT,
+	MemberId				INT NOT NULL AUTO_INCREMENT,
+	Name					VARCHAR(30) NOT NULL, 
+	DOB						DATE,
+	Languages				VARCHAR(100) NOT NULL,
+	IsAssigned				INT,
+	HourlyRate				FLOAT,
+	Role					VARCHAR(20),
+	Experience				INT,
+	SkillScore				INT,
+	AvailableHoursPerWeek	INT,
 	PRIMARY KEY (MemberId)
 );
 
 CREATE TABLE Project(
-	ProjectId	INT NOT NULL AUTO_INCREMENT,
-	Name		VARCHAR(30) NOT NULL,
-	IsAssigned	INT,
-	Size		INT,
-	Budget		FLOAT,
-	Tools		VARCHAR(100),
+	ProjectId				INT NOT NULL AUTO_INCREMENT,
+	Name					VARCHAR(30) NOT NULL,
+	EndDate					DATE,
+	IsAssigned				INT,
+	Size					INT,
+	Budget					FLOAT,
+	Tools					VARCHAR(100),
+	SkillWeight				INT,
+	ExperienceWeight		INT,
+	HoursWeight				INT,
+	LanguageWeight			INT,
+	BudgetWeight			INT,
+	IsAssignmentComplete	INT,
+	Priority				INT,
 	PRIMARY KEY (ProjectId)
 );
 
@@ -24,8 +36,10 @@ CREATE TABLE Team(
 );
 
 CREATE TABLE Requirements(
-	ProjectId	INT NOT NULL REFERENCES Project(ProjectId),
-	Language 	VARCHAR(20) NOT NULL,
-	Skill		INT NOT NULL,
-	PRIMARY KEY(ProjectId)
+	ProjectId				INT NOT NULL REFERENCES Project(ProjectId),
+	MemberNumber			INT,
+	Language 				VARCHAR(20) NOT NULL,
+	Skill					INT NOT NULL,
+	Role					VARCHAR(20),
+	AvailableHoursPerWeek	INT
 );
