09fea770b54e0ed8ae7cbba77e76786a9434fac1
Sameer Thummalapally <sameerthummalapally@sameers-air-2.home>
2020-09-17 14:52:46 -0400

    Singup valid

--- src/Signup.js
+++ src/Signup.js
@@ -2,20 +2,85 @@ import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 import './Signup.css';
 
+
 class Signup extends Component {
+  state ={
+    name: "",
+    nameerror:"", 
+  };
   constructor(props)
   {
     super(props);
     this.state={
-      formflag: this.props.formflag
+      formflag: this.props.formflag,
+      fields: {},
+      errors: {}
     }
+    this.handleChange = this.handleChange.bind(this);
+    // this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
+  };
+  handleChange= (e) =>{
+    e.preventDefault();
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+  //   if (this.validateForm()) {
+  //     this.setState({fields:fields});
+  // }
+  this.validateForm();
   }
-  handlChange= (event) =>{
-    event.preventDefault();
-    const {name, value} = event.target;
-    this.setState({[name]:value});
-    console.log(this.state);
+  validateForm() {
+    let fields = this.state.fields;
+      let errors = {};
+      let formIsValid = true;
+    if (typeof fields["name"] !== "undefined") {
+      if (!fields["name"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        this.state.fields["name"]="";
+        errors["name"] = "*Please enter alphabet characters only.";
+      }
+    }
+    var date = new Date(fields["dob"]);
+    var date2 = new Date(Date.now());
+    var datediff = date2.getFullYear()-date.getFullYear();
+    if(datediff<18 || datediff>65){
+      formIsValid = false;
+      this.state.fields["dob"]="";
+      errors["dob"] = "*Please enter a valid date.";
+    }
+    
+    if (fields["skillscore"]<0 || fields["skillscore"]>100) {
+      formIsValid = false;
+      this.state.fields["skillscore"]="";
+      errors["skillscore"] = "*Please enter a valid score.";
+    }
+    if (fields["experience"]<0 || fields["experience"]>30) {
+      formIsValid = false;
+      this.state.fields["experience"]=0;
+      errors["experience"] = "*Please enter a valid expereince.";
+    }
+    if (fields["hourlyrate"]<0 || fields["hourlyrate"]>250) {
+      formIsValid = false;
+      this.state.fields["hourlyrate"]="";
+      errors["hourlyrate"] = "*Please enter a realistic hourlyrate.";
+    }
+    if (fields["availablehoursperweek"]<0 || fields["availablehoursperweek"]>100) {
+      formIsValid = false;
+      this.state.fields["availablehoursperweek"]="";
+      errors["availablehoursperweek"] = "*Please enter a realistic available hours per week.";
+    }
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
   }
+  // handleSubmit= event =>{
+  //   event.preventDefault();
+  //   console.log(this.state);
+  // }
+
   
   render(){
   return (
@@ -47,25 +112,28 @@ class Signup extends Component {
       <Notify isShow={this.state.formflag} />
       <div className = "test">
         <div className="formblock">
-          <form method= 'post' align = 'center' action = 'http://localhost:3001/Signup'>
+          <form method= 'post' align = 'center' action = 'http://localhost:3001/Signup' >
               <div className="form-row">
                 <div className="form-group col-md-6">
                   <label htmlFor="name" className="form-check-label">Name</label>
-                  <input type="text" className="form-control" name = 'name' required />
+                  <input type="text" value={this.state.fields.name} onChange={this.handleChange} className="form-control" name = 'name' maxLength={30} required/>
+                  <div className="errorMsg">{this.state.errors.name}</div>
                 </div>
                 <div className="form-group col-md-6">
-                  <label htmlFor="dob" className="form-check-label">Date of Birth</label>
-                  <input type="date" className="form-control" name="dob" required/>
+                  <label htmlFor="dob" className="form-check-label">Date of Birth(Min Age = 18)</label>
+                  <input type="date" value={this.state.fields.dob} onChange={this.handleChange} className="form-control" name="dob" required/>
+                  <div className="errorMsg">{this.state.errors.dob}</div>
                 </div>
               </div>
               <div className="form-row">
                 <div className="form-group col-md-6">
-                  <label htmlFor="languages" className="form-check-label">Languages</label>
+                  <label htmlFor="languages" className="form-check-label">Programming Languages</label>
                   <input type="text" className="form-control" name = 'languages' required />
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="hourlyrate" className="form-check-label">Hourly Rate (in dollars)</label>
-                  <input type="number" className="form-control" name="hourlyrate" required/>
+                  <input type="number" value={this.state.fields.hourlyrate} onChange={this.handleChange} className="form-control" name="hourlyrate" required/>
+                  <div className="errorMsg">{this.state.errors.hourlyrate}</div>
                 </div>
               </div>
               <div className="form-row">
@@ -84,20 +152,23 @@ class Signup extends Component {
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="experience" className="form-check-label">Experience (in years)</label>
-                  <input type="number" className="form-control" name="experience" required/>
+                  <input type="number" value={this.state.fields.experience} onChange={this.handleChange} className="form-control" name="experience" required/>
+                  <div className="errorMsg">{this.state.errors.experience}</div>
                 </div>
               </div>
               <div className="form-row">
                 <div className="form-group col-md-6">
-                  <label htmlFor="skillscore" className="form-check-label">Skill/Assesment Score</label>
-                  <input type="number" className="form-control" name = 'skillscore' required/>
+                  <label htmlFor="skillscore" className="form-check-label">Skill/Assesment Score(On a scale of 1-100)</label>
+                  <input type="number" value={this.state.fields.skillscore} onChange={this.handleChange} className="form-control" name = 'skillscore' maxvalue={100} required/>
+                  <div className="errorMsg">{this.state.errors.skillscore}</div>
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="availablehoursperweek" className="form-check-label">Available Hours (per week)</label>
-                  <input type="number" className="form-control" name="availablehoursperweek" required/>
+                  <input type="number" value={this.state.fields.availablehoursperweek} onChange={this.handleChange} className="form-control" name="availablehoursperweek" required/>
+                  <div className="errorMsg">{this.state.errors.availablehoursperweek}</div>
                 </div>
               </div>
-              <input type = 'submit' />
+              <input type = 'submit'/>
             </form>
         </div>
       </div>
