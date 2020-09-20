65e9f3f575517ca05141e7abe6e38b292e7db0ff
lokesh45 <police.lokesh23@gmail.com>
2020-09-20 19:59:12 -0400

    Changes to tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import App from './App';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+<BrowserRouter>
+  <App />
+</BrowserRouter>,
+div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+})
