5a3bbac2b3da76198c883f46a2173e59eab8b9d3
lokesh45 <police.lokesh23@gmail.com>
2020-09-20 19:53:30 -0400

    Changes to tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
