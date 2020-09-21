f5e954ff712884b4be7208b1a281aa7def2798c1
lokesh45 <police.lokesh23@gmail.com>
2020-09-20 20:57:54 -0400

    deploy to github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {
-  BrowserRouter as Router
+  HashRouter as Router
 } from "react-router-dom";
 import './index.css';
 import App from './App';
