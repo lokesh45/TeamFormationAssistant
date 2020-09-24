471be62d32cf9f527df7f8e629a86dfa66dff9f1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-24 19:10:11 +0000

    Deploy lokesh45/TeamFormationAssistant to github.com/lokesh45/TeamFormationAssistant.git:gh-pages

---
+++ precache-manifest.ed7afbfac7e86b606dbd008aeddd216d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a519dc9eb5cf09503640da28d9b490b",
+    "url": "/TeamFormationAssistant/index.html"
+  },
+  {
+    "revision": "1a228735b8a31916bf69",
+    "url": "/TeamFormationAssistant/static/css/main.b3f52b16.chunk.css"
+  },
+  {
+    "revision": "0c47ae4c5a6c540e7cfd",
+    "url": "/TeamFormationAssistant/static/js/2.3e6df6d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TeamFormationAssistant/static/js/2.3e6df6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a228735b8a31916bf69",
+    "url": "/TeamFormationAssistant/static/js/main.8c20aba6.chunk.js"
+  },
+  {
+    "revision": "6a0245738e7094d970d0",
+    "url": "/TeamFormationAssistant/static/js/runtime-main.6ef79560.js"
+  }
+]);
